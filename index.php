7aaf3c98923442f4b8789105c8474de61a1664db
Shakil Shahadat <shakil.regi@gmail.com>
2020-02-17 04:47:50 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>বাংলা কিবোর্ড</title>
 	<link rel="stylesheet" href="src/style.css">
+	<link rel="shortcut icon" href="src/favicon.ico">
 </head>
 <body>
 <script src="src/script.js"></script>
