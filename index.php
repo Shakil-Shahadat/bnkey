c2761f3b18ffb7d949e954dfeac60667695bfe9c
Shakil Shahadat <shakil.regi@gmail.com>
2024-02-06 12:23:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 	<link rel="shortcut icon" href="src/favicon.ico">
 </head>
 <body>
+<div class="container">
+	<textarea cols="70" rows="5" autofocus></textarea>
+	<br>
+</div> <!-- .container -->
+
 <script src="src/script.js"></script>
 </body>
 </html>
