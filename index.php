08484e737cb39627a791959ad41c193435878ae2
Shakil Shahadat <shakil.regi@gmail.com>
2024-02-06 12:24:47 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,77 @@
 	<br>
 
 	<div class="letters">
+		<div class="left">
+			<button onclick="add( this.innerText )">অ</button>
+			<button onclick="add( this.innerText )">আ</button>
+			<button onclick="add( this.innerText )">ই</button>
+			<button onclick="add( this.innerText )">ঈ</button>
+			<button onclick="add( this.innerText )">উ</button>
+			<button onclick="add( this.innerText )">ঊ</button>
+			<br>
+
+			<button onclick="add( this.innerText )">ঋ</button>
+			<button onclick="add( this.innerText )">এ</button>
+			<button onclick="add( this.innerText )">ঐ</button>
+			<button onclick="add( this.innerText )">ও</button>
+			<button onclick="add( this.innerText )">ঔ</button>
+			<button onclick="add( this.innerText )">&nbsp;</button>
+			<hr>
+
+			<button onclick="add( this.innerText )">ক</button>
+			<button onclick="add( this.innerText )">খ</button>
+			<button onclick="add( this.innerText )">গ</button>
+			<button onclick="add( this.innerText )">ঘ</button>
+			<button onclick="add( this.innerText )">ঙ</button>
+			<br>
+
+			<button onclick="add( this.innerText )">চ</button>
+			<button onclick="add( this.innerText )">ছ</button>
+			<button onclick="add( this.innerText )">জ</button>
+			<button onclick="add( this.innerText )">ঝ</button>
+			<button onclick="add( this.innerText )">ঞ</button>
+			<br>
+
+			<button onclick="add( this.innerText )">ট</button>
+			<button onclick="add( this.innerText )">ঠ</button>
+			<button onclick="add( this.innerText )">ড</button>
+			<button onclick="add( this.innerText )">ঢ</button>
+			<button onclick="add( this.innerText )">ণ</button>
+			<br>
+
+			<button onclick="add( this.innerText )">ত</button>
+			<button onclick="add( this.innerText )">থ</button>
+			<button onclick="add( this.innerText )">দ</button>
+			<button onclick="add( this.innerText )">ধ</button>
+			<button onclick="add( this.innerText )">ন</button>
+			<br>
+
+			<button onclick="add( this.innerText )">প</button>
+			<button onclick="add( this.innerText )">ফ</button>
+			<button onclick="add( this.innerText )">ব</button>
+			<button onclick="add( this.innerText )">ভ</button>
+			<button onclick="add( this.innerText )">ম</button>
+			<hr>
+
+			<button onclick="add( this.innerText )">য</button>
+			<button onclick="add( this.innerText )">র</button>
+			<button onclick="add( this.innerText )">ল</button>
+			<button onclick="add( this.innerText )">শ</button>
+			<button onclick="add( this.innerText )">ষ</button>
+			<br>
+
+			<button onclick="add( this.innerText )">স</button>
+			<button onclick="add( this.innerText )">হ</button>
+			<button onclick="add( this.innerText )">ড়</button>
+			<button onclick="add( this.innerText )">ঢ়</button>
+			<button onclick="add( this.innerText )">য়</button>
+			<br>
+
+			<button onclick="add( this.innerText )">ৎ</button>
+			<button onclick="add( this.innerText )">ং</button>
+			<button onclick="add( this.innerText )">ঃ</button>
+			<button onclick="add( this.innerText )">ঁ</button>
+		</div> <!-- .left -->
 	</div> <!-- .letters -->
 </div> <!-- .container -->
 
